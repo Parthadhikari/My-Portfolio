7198d4ce6aa9f62b12ef30be40361ed77c5e6c7f
Parth <parthadhikari1812@gmail.com>
2023-03-30 03:36:14 +0530

    Seventh commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.scss';
-// import { Route, Routes } from 'react-router-dom';
-import { Route, Switch } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
@@ -9,24 +7,15 @@ import Contact from './components/Contact';
 
 function App() {
   return(
-   //   <>
-   //      <Routes>
-   //         <Route path="/" element={<Layout />}>
-   //            <Route index element={<Home />}/>
-   //            <Route path='about' element={<About />} />
-   //            <Route path='contact' element={<Contact />} />
-   //         </Route>
-   //      </Routes>
-   //   </>
-   <HashRouter>
-      <Layout>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-        </Switch>
-      </Layout>
-    </HashRouter>
+     <>
+        <Routes>
+           <Route path="/" element={<Layout />}>
+              <Route index element={<Home />}/>
+              <Route path='about' element={<About />} />
+              <Route path='contact' element={<Contact />} />
+           </Route>
+        </Routes>
+     </>
    );
 }
 
